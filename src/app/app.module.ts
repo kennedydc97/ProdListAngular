27c5a2b083e6fd28f979fe3f6cad5ed01af0b912
Treinamento <Treinamento@RD723911.raiadrogasil.com>
2020-03-05 18:29:08 -0300

    lista de produtos angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListaDeProdutosComponent } from './components/lista-de-produtos/lista-de-produtos.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaDeProdutosComponent,
+    CategoriasComponent,
+    NavComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
