dfbf8f5b6438b3111bde3e3843eded6d63348e9e
Kennedy <kennedy_dourado@hotmail.com>
2020-03-05 23:25:46 -0300

    pequeno avanço na lista de produtos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Categoria } from 'src/app/models/categorias';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+
+  categoriaSelecionada(categoria: Categoria){
+    console.log(categoria);
+  }
 
   ngOnInit(): void {
   }
